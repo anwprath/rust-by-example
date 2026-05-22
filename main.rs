10816a42e9a02e5f2e33faa710255e50a9d6c4e9
Prathamesh Anwekar <pratham.2118@gmail.com>
2026-05-22 21:32:30 +0530

    enums

--- main.rs
+++ main.rs
@@ -1,49 +1,33 @@
 use std::{fmt, string};
 
-#[derive(PartialEq, Debug)]
-struct Unit; // Go equivalent of type Unit struct{}
+// What has Go done to me
+// I can't tell if I hate or love these enums
+enum Event {
+    PageLoad,
+    PageUnload,
 
-// A tuple struct
-struct Pair(i32, f32);
+    Click(char),
+    Scroll(i128),
 
-// A struct with two fields
-struct Point {
-    x: f32,
-    y: f32,
+    Position { x: i128, y: i128 },
 }
 
-struct Rectangle {
-    top_left: Point,
-    bottom_right: Point,
+fn inspect(e: Event) {
+    match e {
+        Event::PageLoad => println!("PageLoad"),
+        Event::PageUnload => println!("PageUnLoad"),
+        Event::Click(c) => println!("Clicked {}", c),
+        Event::Scroll(f) => println!("Scrolled {} pixels", f),
+        Event::Position { x, y } => println!("At position: ({}, {})", x, y),
+    }
 }
 
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8
-}
-
-
+type String = Event; // Hello type aliases
 
 fn main() {
-    let person = Person {name: String::from("Dumbass"), age:2};
-    println!("{:#?}", person);
+    let load = Event::PageLoad;
+    let position = String::Position { x: 2, y: (3) };
 
-    let point: Point = Point { x: 5.2, y: 0.4 };
-    let another_point: Point = Point { x: 10.3, y: 0.2 };
-
-    // Access the fields of the point
-    println!("point coordinates: ({}, {})", point.x, point.y);
-
-    // Make a new point by using struct update syntax to use the fields of our
-    // other one
-    let bottom_right = Point { x: 12.3, ..another_point };
-    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
-
-    let unit1 = Unit{};
-    let unit2 = Unit{};
-
-    assert_eq!(unit1, unit2); // Point to different objects though
-    println!("{:p}, {:p}", &unit1, &unit2);
-
+    inspect(load);
+    inspect(position);
 }
